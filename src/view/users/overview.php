1b6e3f93d8da94d24d46bfe2b1e0f6675960307d
KenPN <kenny.nguyen2@student.howest.be>
2021-11-24 10:58:27 +0100

    Created group overview and styled it

--- src/view/users/overview.php
+++ src/view/users/overview.php
@@ -1,5 +1,40 @@
 <section class="content">
-<p>Welcome <?php echo $_SESSION['name']?></p>
+<h2 class="overview-header">Welcome <?php echo $_SESSION['name']?></h2>
 
+<hr>
+<div class="group-list">
+  <span class="group-heading">My Groups</span>
+
+  <a href="#">
+    <div class="group-entry">
+      <figure></figure>
+      <div>
+        <span>Group name: </span> <br>
+        <span>Upcoming event: </span>
+      </div>
+    </div>
+  </a>
+
+  <a href="#">
+    <div class="group-entry">
+      <figure></figure>
+      <div>
+        <span>Group name: </span> <br>
+        <span>Upcoming event: </span>
+      </div>
+    </div>
+  </a>
+
+  <a href="#">
+    <div class="group-entry">
+      <figure></figure>
+      <div>
+        <span>Group name: </span> <br>
+        <span>Upcoming event: </span>
+      </div>
+    </div>
+  </a>
+
+</div>
 
 </section>
