891dd9bde083c1eaa0df3d4367729d7338001e44
Jenneeee <jenne@cattoor.net>
2021-12-06 13:58:26 +0100

    Changes to database connection after error

--- src/bootstrap/database.php
+++ src/bootstrap/database.php
@@ -3,10 +3,10 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 $capsule->addConnection([
  'driver'    => 'mysql',
- 'host'      => getenv('PHP_DB_HOST') ?: 'mysql',
- 'database'  => getenv('PHP_DB_DATABASE') ?: 'plan_it',
- 'username'  => getenv('PHP_DB_USERNAME') ?: 'root',
- 'password'  => getenv('PHP_DB_PASSWORD') ?: 'devine4life',
+ 'host'      => getenv('PHP_DB_HOST') ?: 'ID338682_jennecattoorbe.db.webhosting.be',
+ 'database'  => getenv('PHP_DB_DATABASE') ?: 'ID338682_jennecattoorbe',
+ 'username'  => getenv('PHP_DB_USERNAME') ?: 'ID338682_jennecattoorbe',
+ 'password'  => getenv('PHP_DB_PASSWORD') ?: 'J7T35LcCepm7mSA2',
  'charset'   => 'utf8mb4',
  'collation' => 'utf8mb4_unicode_ci',
  'prefix'    => '',
